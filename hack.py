4f3a04e0dcf6b84d12270e99c0be677f9b998ea4
Onat Ender <onatender@gmail.com>
2024-04-29 00:39:19 +0300

    tkinter interface added

--- hack.py
+++ hack.py
@@ -1,3 +1,6 @@
+import time
+from tkinter import *
+import tkinter
 from pymem import Pymem
 from pymem.ptypes import RemotePointer
 
@@ -14,5 +17,25 @@ def getPointerAddress(base, offsets):
             remote_pointer = RemotePointer(pm.process_handle, remote_pointer.value + offset)
         else:
             return remote_pointer.value + offset
-while not keyboard.is_pressed('q'):
-        pm.write_int(getPointerAddress(pm.base_address + 0x03674F10,offsets=offsets),1000)
+# while not keyboard.is_pressed('q'):
+#         pm.write_int(getPointerAddress(pm.base_address + 0x03674F10,offsets=offsets),1000)
+
+gui = Tk()
+gui.title("Brotato Hile")
+Label(text="Para").grid(row=0,column=0)
+strvar = StringVar()
+textbox = Entry(gui, width=15,textvariable=strvar,state="readonly")
+textbox.grid(row=0,column=1)
+Label(text="Yeni Para").grid(row=1,column=0)
+strvar2 = StringVar()
+textbox2 = Entry(gui, width=15,textvariable=strvar2)
+textbox2.grid(row=1,column=1)
+def yap():
+    pm.write_int(getPointerAddress(pm.base_address + 0x03674F10,offsets=offsets),int(strvar2.get()))
+btn = Button(gui,text="Yap",command=yap) 
+btn.grid(row=2,column=0,columnspan=2)
+
+while True:
+    strvar.set(pm.read_int(getPointerAddress(pm.base_address + 0x03674F10,offsets=offsets)))
+    gui.update()
+    
